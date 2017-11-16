ca6a079d178e976b2cf6a3fb9c344bc03b9f18ca
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-16 10:56:22 -0800

    Adds root route

--- skeleton/frontend/pokedex.jsx
+++ skeleton/frontend/pokedex.jsx
@@ -4,6 +4,8 @@ import {fetchAllPokemon} from './util/api_util';
 import {receiveAllPokemon} from './actions/pokemon_actions';
 import configureStore from './store/store';
 import Root from './components/root'
+import { HashRouter, Route } from 'react-router-dom';
+
 
 
 // window.receiveAllPokemon = receiveAllPokemon;
